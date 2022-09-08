6476077a18b1228cc958873cd17014a9cbf94d24
juanOpp <sandra.lozanosanchez@plexus.local>
2022-09-08 13:19:05 +0200

    init

---
+++ src/utilsOpp/UtilsOpp.java
@@ -0,0 +1,16 @@
+package utilsOpp;
+
+import java.io.Serializable;
+
+public final class UtilsOpp implements Serializable {
+
+	private static final long serialVersionUID = -6104248078873460984L;
+	
+	private UtilsOpp() {
+	}
+
+	public static double suma(double sum1, double sum2) {
+		return sum1 + sum2;
+	}
+
+}
